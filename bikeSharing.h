880adc1f54214890ae5c0575a8f260f3140410a5
smaffeo33 <smaffeo@itba.edu.ar>
2023-07-02 19:53:06 -0300

    This commit will add standard and generic functions, as well as define what the bikesharingADT is

---
+++ bikeSharing.h
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#ifndef __bikeSharing_h
+#define __bikeSharing_h
+
+typedef struct bikeSharingCDT * bikeSharingADT;
+
+
+/*libera toda memoria reservada por el TAD*/
+void freeBikeSharing(bikeSharingADT bikeSharing);
+
+/*Retorna un nuevo bikesharing. Al principio esta vacio*/
+bikeSharingADT newBikeSharing();
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
