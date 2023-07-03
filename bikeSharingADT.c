b368fb5c1b34123d7004d752fbc62f10141f63f0
josebl123 <jobenegaslynch@itba.edu.ar>
2023-07-03 19:13:38 -0300

    this commit will, hopefully, save us

--- bikeSharingADT.c
+++ bikeSharingADT.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-<<<<<<< HEAD
 
 
 typedef struct node {
@@ -14,16 +13,9 @@ typedef struct node {
 }TNode;
 
 typedef TNode * TList;
-=======
 #include "bikeSharing.h"
->>>>>>> ec4f8b0c6aa39ae32893388e10c65f1aa40964e0
 
 typedef struct bikeSharingCDT {
     
 
-<<<<<<< HEAD
 }bikeSharingCDT;
-
-=======
-}bikeSharingCDT;
->>>>>>> ec4f8b0c6aa39ae32893388e10c65f1aa40964e0
