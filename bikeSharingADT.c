ea5e50b5c4fdd18eeec1ae4c673d219066267527
leoWeitz <lweitz@itba.edu.ar>
2023-07-03 23:07:48 -0300

    Set up a possible way of storing information within the TAD

--- bikeSharingADT.c
+++ bikeSharingADT.c
@@ -2,20 +2,32 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stddef.h>
+#include "bikeSharing.h"
 
+#define MAX_LETTERS 60
+#define MONTHS 12
+
+static enum months {JAN = 0, FEB, MAR, APR, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
+
+typedef struct trips {
+    char end_station_name[MAX_LETTERS];
+    int year;
+} Ttrip;
 
 typedef struct node {
     int id;
     double latitude;
     double longitude;
-    char station_name[60];
+    char station_name[MAX_LETTERS];
+    size_t total_trips; //Viajes comenzados en esta estacion
+    size_t trips_per_month[MONTHS];
+    Ttrip * first; //Lista de estaciones que tienen un trip de la forma node->estacion
     struct node  * tail;
 }TNode;
 
 typedef TNode * TList;
-#include "bikeSharing.h"
 
 typedef struct bikeSharingCDT {
-    
-
+    TList first; // Primera estacion
 }bikeSharingCDT;
