fd3e8b32557905890fcb1b67c2e4abd147b12bb7
smaffeo33 <smaffeo@itba.edu.ar>
2023-07-04 20:13:05 -0300

    This commit will add new prototypes for functions in the .h

--- bikeSharing.h
+++ bikeSharing.h
@@ -8,26 +8,6 @@
 typedef struct bikeSharingCDT * bikeSharingADT;
 
 
-
-//struct * makeIdArray()  crea un vector que relaciona el id, el nombre, los viajes de los miembros, y un vector de 12 posiciones que contenga la cant de viajes x mes de las estaciones con una posicion de la matriz (chequear toArray)
-
-// hacer la matriz al mismo tiempo 
-
-// int getIndex()  recorre el vector y cuando matchea el id de la estacion devuelve la posicion en la matriz
-
-// addtrip deberia cargar el viaje de destino y origen a la matriz
-
-// q1() crea un vector nuevo igual al Idarray y lo ordena descendentemente (sin id)
-
-// q2() va a la matriz y obtiene los valores ij y ji. ignora i = j. llama a getdata y va a los indices solicitados.
-
-// q3() recorre el IdArray, va al vector de los viajes de los meses y saca la info ( ya esta ordenado alfabeticamente).
-
-// q4() va a la matriz, agarra la diagonal, lo pasa a un vector, y llama a la funcion de orden para ordenar ese vector. (descendente)
-
-// order() ...
-
-
 /*libera toda memoria reservada por el TAD*/
 void freeBikeSharing(bikeSharingADT bikeSharing);
 
@@ -37,7 +17,7 @@ bikeSharingADT newBikeSharing();
 /*Almacena una nueva estacion en orden alfabetico el TAD*/
 void addStation(bikeSharingADT bikeSharing, char * station, int id);
 
-/*Almacena un nuevo viaje*/
+/*Almacena un nuevo viaje. Addtrip deberia cargar el viaje de destino y origen a la matriz*/ 
 void addTrip(bikeSharingADT bikeSharing, char isMember, size_t startId, size_t endId, int year, int month);
 
 /*Se setea que vamos a empezar a recorrer desde el primer nodo*/
@@ -48,6 +28,32 @@ size_t hasNext(const bikeSharingADT bikeSharing);
 
 /*apunta a la siguiente posición de la lista, solo si hasNext devolvio ture*/
 void next(bikeSharingADT bikeSharing); 
+
+//chequear que devuelven las funciones de query(tiene sentido que devuelva un nuevo vector para podedr imprimir lindo)
+
+/* crea un vector nuevo igual al Idarray y lo ordena descendentemente (sin id)*/
+int * q1(); 
+
+/* va a la matriz y obtiene los valores ij y ji. ignora i = j. llama a getdata y va a los indices solicitados. */
+int * q2(); 
+ 
+ /* recorre el IdArray, va al vector de los viajes de los meses y saca la info ( ya esta ordenado alfabeticamente).*/
+int * q3();
+
+/*va a la matriz, agarra la diagonal, lo pasa a un vector, y llama a la funcion de orden para ordenar ese vector. (descendente) */
+int * q4();
+
+/*crea un vector que relaciona el id, el nombre, los viajes de los miembros, y un vector de 12 posiciones que contenga la cant de viajes x mes de las estaciones con una posicion de la matriz (chequear toArray)
+ hacer la matriz al mismo tiempo 
+ */
+struct * makeIdArray(); //esta mal como definimos lo qeu devuelve. 
+
+/*recorre el vector y cuando matchea el id de la estacion devuelve la posicion en la matriz*/
+int getIndice();
+
+/*ordena de manera descendientemente por cantidad de viajes*/
+void sort();
+
 
 
 #endif
