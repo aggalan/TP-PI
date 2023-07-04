54b88d5ec9a5ac1807515771695386ac1d4c602c
smaffeo33 <smaffeo@itba.edu.ar>
2023-07-04 16:42:02 -0300

    this commit will add two new functions

--- bikeSharing.h
+++ bikeSharing.h
@@ -12,6 +12,11 @@ void freeBikeSharing(bikeSharingADT bikeSharing);
 /*Retorna un nuevo bikesharing. Al principio esta vacio*/
 bikeSharingADT newBikeSharing();
 
+/*Almacena una nueva estacion en el TAD*/
+void addStation(bikeSharingADT bikeSharing, char * station, int id);
+
+/*Almacena un nuevo viaje*/
+void addTrip(bikeSharingADT bikeSharing, char isMember, char station_name[], char end_station_name[], int year, int month);
 
 
 
