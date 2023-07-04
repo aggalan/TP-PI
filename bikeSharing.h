203879f334d51e3169bf68be636927068decebce
smaffeo33 <smaffeo@itba.edu.ar>
2023-07-04 17:19:33 -0300

    this commit will add more functions to the .h

--- bikeSharing.h
+++ bikeSharing.h
@@ -14,16 +14,26 @@ void freeBikeSharing(bikeSharingADT bikeSharing);
 /*Retorna un nuevo bikesharing. Al principio esta vacio*/
 bikeSharingADT newBikeSharing();
 
-/*Almacena una nueva estacion en el TAD*/
+/*Almacena una nueva estacion en orden alfabetico el TAD*/
 void addStation(bikeSharingADT bikeSharing, char * station, int id);
 
 /*Almacena un nuevo viaje*/
 void addTrip(bikeSharingADT bikeSharing, char isMember, size_t startId, size_t endId, int year, int month);
 
+/*Ordena la lista por cantidad total de viajes*/
+void sortByTrips(bikeSharingADT bikeSharing);
 
+/*Ordena la lista por cantidad de viajes circulares*/
+void sortByCircularTrips(bikeSharingADT bikeSharing);
 
+/*Se setea que vamos a empezar a recorrer desde el primer nodo*/
+void toBegin(bikeSharingADT bikeSharing);
 
+/*retorna true si hay algo pendiente por recorrer*/
+size_t hasNext(const bikeSharingADT bikeSharing);
 
+/*me devuelve el siguiente, solo si hasNext devolvio ture*/
+bikeSharingADT next(bikeSharingADT bikeSharing);
 
 
 
