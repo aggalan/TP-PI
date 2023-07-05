5e6680ae173441bc6611391bac46c15f5451876d
leoWeitz <lweitz@itba.edu.ar>
2023-07-05 17:52:45 -0300

    Modify the .h, queiries functions

--- bikeSharing.h
+++ bikeSharing.h
@@ -2,11 +2,28 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <string.h>
+#define MONTHS 12
 #ifndef __bikeSharing_h
 #define __bikeSharing_h
 
 typedef struct bikeSharingCDT * bikeSharingADT;
 
+typedef struct q1_struct {
+    char * station_name;
+    size_t trips;
+} q1_struct;
+
+typedef struct q2_struct {
+    char * start_station;
+    char * end_station;
+    size_t trips_start-end;
+    size_t trips_end-start;
+} q2_struct;
+
+typedef struct q3_struct {
+    char * station_name;
+    size_t months[MONTHS];
+} q3_struct;
 
 /*libera toda memoria reservada por el TAD*/
 void freeBikeSharing(bikeSharingADT bikeSharing);
@@ -23,17 +40,33 @@ void addTrip(bikeSharingADT bikeSharing, char isMember, size_t startId, size_t e
 
 //chequear que devuelven las funciones de query(tiene sentido que devuelva un nuevo vector para podedr imprimir lindo)
 
-/* crea un vector nuevo igual al Idarray y lo ordena descendentemente (sin id)*/
-int * q1(); 
+/* 
+   Crea un vector nuevo igual al Idarray y lo ordena descendentemente (sin id).
+   Retorna un vector de structs del estilo: 
+   struct q1_struct {
+    char * station_name;
+    size_t trips;
+   }
+*/
+struct q1_struct * q1(); 
 
-/* va a la matriz y obtiene los valores ij y ji. ignora i = j. llama a getdata y va a los indices solicitados. */
-int * q2(); 
+/* 
+    Va a la matriz y obtiene los valores ij y ji. ignora i = j. llama a getdata y va a los indices solicitados. 
+    Retorna un struct del estilo q2_struct.
+*/
+q2_struct * q2(); 
  
- /* recorre el IdArray, va al vector de los viajes de los meses y saca la info ( ya esta ordenado alfabeticamente).*/
-int * q3();
+ /* 
+    Recorre el IdArray, va al vector de los viajes de los meses y saca la info ( ya esta ordenado alfabeticamente).
+    Retorna un vector de structs del estilo q3_struct;
+ */
+q3_struct * q3();
 
-/*va a la matriz, agarra la diagonal, lo pasa a un vector, y llama a la funcion de orden para ordenar ese vector. (descendente) */
-int * q4();
+/*
+    Va a la matriz, agarra la diagonal, lo pasa a un vector, y llama a la funcion de orden para ordenar ese vector. (descendente) 
+    Retorna un vector de struct del estilo q1_struct.    
+*/
+q1_struct * q4();
 
 /*asigna la memoria necesaria al vector y la matriz (bikeSharing->cant) con calloc, y haria un recorrido de la lista llenando los campos de id y de station_name*/
 
