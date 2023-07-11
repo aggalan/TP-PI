0aa46b91145b849e2f18d80c319e316a0ca8ea90
smaffeo33 <smaffeo@itba.edu.ar>
2023-07-11 17:56:16 -0300

    fixes .h comments

--- bikeSharing.h
+++ bikeSharing.h
@@ -28,37 +28,38 @@ typedef struct q3_struct {
 /*Frees up the resourdces used by the ADT*/
 void freeBikeSharing(bikeSharingADT bs);
 
-// Frees up the resources used by a q1_struct style vec
+// Frees up the resources used by a q1_struct style array
 void freeVec1(bikeSharingADT bs, q1_struct * vec1);
 
-// Frees up the resources used by a q2_struct style vec
+// Frees up the resources used by a q2_struct style array
 void freeVec2(bikeSharingADT bs, q2_struct * vec2, int dim);
 
-// Frees up the resources used by a q3_struct style vec
+// Frees up the resources used by a q3_struct style array
 void freeVec3(bikeSharingADT bs, q3_struct * vec3);
 
 /*Returns a new bikeSharing, which is empty at first*/
 bikeSharingADT newBikeSharing(int start_year, int end_year);
 
-/*Stores a new station in alphabetical order in the TAD. Returns int to check for memory errors*/
+/*Stores a new station in alphabetical order in the TAD. Returns a boolean character to check for memory allocation errors.*/
 int addStation(bikeSharingADT bikeSharing, char * station, int id);
 
 /*
 Reserves space for the matrix and sets the indexes.
 It must be called before starting to load the trips.
+Returns a boolean character to check for memory allocation errors.
 */
 int setMatrix(bikeSharingADT bs, int * cant);
 
-/*Almacena un nuevo viaje. Addtrip deberia cargar el viaje de destino y origen a la matriz*/ 
+/*Stores a new trip. Addtrip should load the source trip and the destination trip to the matrix*/ 
 void addTrip(bikeSharingADT bikeSharing, int isMember, int startId, int endId, int year, int month);
 
 
-//chequear que devuelven las funciones de query(tiene sentido que devuelva un nuevo vector para podedr imprimir lindo)
+
 
 /* 
-   Crea un vector nuevo igual al Idarray y lo ordena descendentemente (sin id).
-   Retorna un vector de structs del estilo: 
-   struct q1_struct {
+Creates a new array that is the same as Idarray and sorts it in descending order (without id).
+Returns a vector of structs of the style:
+struct q1_struct {
     char * station_name;
     size_t trips;
    }
@@ -66,14 +67,14 @@ void addTrip(bikeSharingADT bikeSharing, int isMember, int startId, int endId, i
 q1_struct * q1(bikeSharingADT bikeSharing, int query); 
 
 /* 
-    Va a la matriz y obtiene los valores ij y ji. ignora i = j. llama a getdata y va a los indices solicitados. 
-    Retorna un struct del estilo q2_struct.
+    Acceses the matrix and gets the ij and ji values. Ignores i = j. Calls getdata and goes to the requested indexes.
+    returns a struct of the style q2_struct.
 */
 q2_struct * q2(bikeSharingADT bikeSharing, int * dim); 
  
  /* 
-    Recorre el IdArray, va al vector de los viajes de los meses y saca la info ( ya esta ordenado alfabeticamente).
-    Retorna un vector de structs del estilo q3_struct;
+    Gets the information out of the monthly travel array, by first looking up the id in the idArray
+    Returns and array of structs of the style q3_struct.
  */
 q3_struct * q3(bikeSharingADT bikeSharing);
 
